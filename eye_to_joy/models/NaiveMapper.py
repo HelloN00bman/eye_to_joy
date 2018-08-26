d371111f43e7c01524b425323cd39d3db40e7739
HelloN00bman <Ben@VPN-128-237-147-53.LIBRARY.VPN.CMU.EDU>
2018-08-26 13:37:29 -0400

    fix lstm

--- eye_to_joy/models/NaiveMapper.py
+++ eye_to_joy/models/NaiveMapper.py
@@ -1,52 +1,58 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as autograd
 import torchvision.models as models
+import functools as ft
 
-from .EgoEncoder import *
-from .GazeEncoderMS import *
+def weight_init(layer, genre, init_func):
+	print(layer, genre)
+	if isinstance(layer, genre):
+		init_func(layer.weight)
 
-WINDOWS = [12, 24, 36]
-WINDOW_SIZE = max(WINDOWS)
+def bias_init(layer, genre, init_func):
+	if isinstance(layer, genre):
+		init_func(layer.bias)
 
 class NaiveMapper(nn.Module):
-	def __init__(self, bsz=10):
+	def __init__(self, batch_size=10, hidden_size=512):
 		super(NaiveMapper, self).__init__()
-		self.hidden_dim = 512
-		self.minibatch = bsz
-		self._make_features()
-		self._make_classifier()
+		self.hidden_dim = hidden_size
+		self.minibatch = batch_size
+		
+		self.hidden = self.init_hidden()
+		
+		self.features = self._make_features()
+		self.classifier = self._make_classifier()
+		
+		self._init_weights_and_biases()
 
 	def init_hidden(self):
-		if not torch.cuda.is_available():
-			return (autograd.Variable(torch.zeros(2, self.minibatch, self.hidden_dim).float()),
-				autograd.Variable(torch.zeros(2, self.minibatch, self.hidden_dim).float()))
-		else:
-			return (autograd.Variable(torch.zeros(2, self.minibatch, self.hidden_dim).float().cuda()),
-				autograd.Variable(torch.zeros(2, self.minibatch, self.hidden_dim).float().cuda()))
+		hidden = (
+			autograd.Variable(torch.zeros(2, self.minibatch, self.hidden_dim).float()),
+			autograd.Variable(torch.zeros(2, self.minibatch, self.hidden_dim).float())
+		)
+		
+		hidden = [l.cuda() if torch.cuda.is_available() else l for l in hidden]
+
+		return hidden
 
 	def _init_weights_and_biases(self):
 		init_w = nn.init.xavier_normal_
 		init_b = ft.partial(nn.init.constant_, val=0)
 		
-		for param in self.lstm.parameters():
+		for param in self.features.parameters():
 			if len(param.shape) >= 2:
 				init_w(param.data)
 			else:
-				init_b(param.data, 0)
+				init_b(param.data)
 
-		self.classifier.apply(ft.partial(weight_init, nn.Linear, init_w))
-		self.classifier.apply(ft.partial(bias_init, nn.Linear, init_b))
+		self.classifier.apply(ft.partial(weight_init, genre=nn.Linear, init_func=init_w))
+		self.classifier.apply(ft.partial(bias_init, genre=nn.Linear, init_func=init_b))
 
 	def _make_features(self):
-		self.ego = models.vgg13_bn(True) # pretained VGG model w/ batchnorm
-		self.ego.eval()
-
-		self.gaze = GazeEncoderMS(*WINDOWS)
-
-		self.lstm = nn.LSTM(input_size=512*7*7 + 1024*3, hidden_size=self.hidden_dim, num_layers=2)
+		lstm = nn.LSTM(input_size=512*7*7 + 1024*3, hidden_size=self.hidden_dim, num_layers=2)
+		return lstm
 
 	def _make_classifier(self):
 		num_classes = 257
@@ -58,25 +64,11 @@ class NaiveMapper(nn.Module):
 			nn.ReLU(True),
 			nn.Linear(1024,num_classes)
 			)
-		self.classifier = layers
 		return layers
 
-	def forward(self, x, hidden):
-		lstm_out, lstm_hidden = self.lstm(x, hidden)
-		out = self.classifier(lstm_out.view(-1, lstm_out.size(2)))
-		# out = F.log_softmax(out)
-		out1 = out[:,:,:256]
-		# out2 = nn.Sequential(nn.Sigmoid())(out[:,:,2])
-		out2 = out[:,:,256]
-		return out1, out2, lstm_hidden
-
-		# ego_out = torch.unbind(ego.view(1, 1, -1))[0]
-		# print('shapes')
-		# print(ego_out.shape)
-		# print(gaze.shape)
-		# ego_gaze = torch.unsqueeze(torch.cat((ego_out, gaze),1),0)
-		# print(ego_gaze.shape)
-		# lstm_out, self.lstm_hidden = self.lstm(ego_gaze, self.hidden)
-		# lstm_out = lstm_out.view(1, -1)
-		# out = self.classifier(lstm_out)
-		# return out
+	def forward(self, x):
+		lstm_out, hidden = self.features(x, self.hidden)
+		feat_out = self.classifier(lstm_out.view(-1, lstm_out.size(2)))
+		pos = feat_out[:,:256]
+		mode = out[:,256]
+		return pos, mode, hidden
