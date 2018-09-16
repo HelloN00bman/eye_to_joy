dd1df1a208c041f1b39ca2693ab2eaf15cefddaa
HelloN00bman <newmanba@indiana.edu>
2018-09-16 14:30:03 -0400

    update model

--- eye_to_joy/models/NaiveMapperHeatmap.py
+++ eye_to_joy/models/NaiveMapperHeatmap.py
@@ -66,7 +66,7 @@ class NaiveMapperHeatmap(nn.Module):
 
 
 	def _make_features(self):
-		lstm = nn.LSTM(input_size=512*7*7 + 1024*3, hidden_size=self.hidden_dim, num_layers=2)
+		lstm = nn.LSTM(input_size=512*14*14 + 1024*3, hidden_size=self.hidden_dim, num_layers=2)
 		return lstm
 
 
