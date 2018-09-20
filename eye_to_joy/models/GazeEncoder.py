dec6dd50b9bf77cff80412e71f408a0b4fd3298a
HelloN00bman <newmanba@indiana.edu>
2018-09-20 11:11:40 -0400

    adsf

--- eye_to_joy/models/GazeEncoder.py
+++ eye_to_joy/models/GazeEncoder.py
@@ -72,7 +72,7 @@ class GazeEncoder(nn.Module):
 
 	def forward(self, x):
 		print(x.shape)
-		x = self.features(x).view(360, -1)
+		x = self.features(x).view(10, -1)
 		# x = x.view(-1, int(64*self.w/4))
 		# x = x.view(-1)
 		
